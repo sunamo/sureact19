5e6a6e6ab0e985ef9f3061fe0fa159202e3a469c
sunamo.cz <radek.jancik@sunamo.cz>
2025-06-16 01:06:43 +0200

    .

---
+++ src/components/headers/H1.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+type Props = {
+    notUnderlineColor?: boolean;
+    children: React.ReactNode;
+};
+export declare function H1({ notUnderlineColor, children }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
